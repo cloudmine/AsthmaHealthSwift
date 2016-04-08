ff86856d84438584fd7df86bf7fae1a1cfa0d68e
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-04-08 13:51:23 -0400

    Add the main panel and profile screen layout, and load it conditionally if the user is already logged in

--- AsthmaHealthSwift/SupportingFiles/AppDelegate.swift
+++ AsthmaHealthSwift/SupportingFiles/AppDelegate.swift
@@ -6,8 +6,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        return true
+    }
+
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         CMHealth.setAppIdentifier(Secrets.AppId, appSecret: Secrets.APIKey)
+
+        if CMHUser.currentUser().isLoggedIn {
+            print("Logged in as \(CMHUser.currentUser())")
+            load(storyboard: "MainPanel")
+        } else {
+            load(storyboard: "Onboarding")
+        }
+
         return true
     }
 
@@ -29,6 +42,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
 
+    }
+
+    func load(storyboard name: String) {
+        let vc = UIStoryboard(name: name, bundle: nil).instantiateInitialViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
     }
 }
 
