dfa71f679c2b54d22b5b65b9007369de6ca86a2b
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-04-13 13:03:22 -0400

    Display user data in profile screen

--- AsthmaHealthSwift/ViewControllers/ProfileViewController.swift
+++ AsthmaHealthSwift/ViewControllers/ProfileViewController.swift
@@ -3,11 +3,22 @@ import CMHealth
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var logOutButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         logOutButton.setBorder(width: 1.0, corderRadius: 4.0)
+        nameLabel.text = ""
+        emailLabel.text = ""
+
+        CMHUser.currentUser().addObserver(self, forKeyPath: "userData", options: NSKeyValueObservingOptions.Initial.union(.New), context: nil)
+    }
+
+    deinit {
+        CMHUser.currentUser().removeObserver(self, forKeyPath: "userData")
     }
 }
 
@@ -27,6 +38,36 @@ private extension ProfileViewController {
             }
 
             appDelegate.loadOnboarding()
+        }
+    }
+}
+
+// MARK: KVO
+
+extension ProfileViewController {
+
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        guard let user = object as? CMHUser,
+            let keyPath = keyPath where keyPath == "userData",
+            let userData = user.userData else {
+                return
+        }
+
+        updateUI(with: userData)
+    }
+}
+
+// MARK: Private Helpers
+
+private extension ProfileViewController {
+    func updateUI(with userData: CMHUserData) {
+        onMainThread {
+            self.emailLabel.text = userData.email
+
+            if let given = userData.givenName,
+                let family = userData.familyName {
+                self.nameLabel.text = "\(given) \(family)"
+            }
         }
     }
 }
